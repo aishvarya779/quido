3f14ddc79866cf07436ad43dab6813a36ccc5fa7
aishvarya779 <aishvaryatiwari123@gmail.com>
2025-03-20 20:08:18 +0530

    first lynx commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import arrow from './assets/arrow.png'
 import lynxLogo from './assets/lynx-logo.png'
 import reactLynxLogo from './assets/react-logo.png'
 
-export function App() {
+export const App = () => {
   const [alterLogo, setAlterLogo] = useState(false)
 
   useEffect(() => {
@@ -27,15 +27,14 @@ export function App() {
               ? <image src={reactLynxLogo} className='Logo--react' />
               : <image src={lynxLogo} className='Logo--lynx' />}
           </view>
-          <text className='Title'>React</text>
+          <text className='Title'>Taste New React</text>
           <text className='Subtitle'>on Lynx</text>
         </view>
         <view className='Content'>
           <image src={arrow} className='Arrow' />
           <text className='Description'>Welcome Aishvarya!</text>
           <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
-            to see updates!
+            New VErsion
           </text>
         </view>
         <view style={{ flex: 1 }}></view>
